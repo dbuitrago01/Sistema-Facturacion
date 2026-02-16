cfc4d7046c711663aa03a56c711a19663719d425
dbuitrago01 <daniel.buitrago955@hotmail.com>
2026-02-15 23:51:16 -0500

    update dashboad

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,31 +13,31 @@
         <div class="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
 
             <a href="{{ route('items.index') }}" class="bg-white p-6 rounded-lg shadow hover:shadow-lg transition">
-                <h3 class="text-lg font-bold">Items</h3>
+                <h3 class="text-lg font-bold">🧴 Items</h3>
                 <p class="text-gray-600">Servicios y productos</p>
             </a>
 
             <a href="{{ route('facturacion.index') }}" class="bg-white p-6 rounded-lg shadow hover:shadow-lg transition">
-                <h3 class="text-lg font-bold">Facturación</h3>
+                <h3 class="text-lg font-bold">💰 Facturación</h3>
                 <p class="text-gray-600">Caja y ventas</p>
             </a>
 
             <a href="{{ route('stock.index') }}" class="bg-white p-6 rounded-lg shadow hover:shadow-lg transition">
-                <h3 class="text-lg font-bold">Stock</h3>
+                <h3 class="text-lg font-bold">📦 Stock</h3>
                 <p class="text-gray-600">Inventario</p>
             </a>
             <a href="{{ route('stock.movimientos') }}" class="bg-white p-6 rounded-lg shadow hover:shadow-lg transition">
-                <h3 class="text-lg font-bold">Historial</h3>
+                <h3 class="text-lg font-bold">📜 Historial</h3>
                 <p class="text-gray-600">Movimientos Transaccionales</p>
             </a>
 
              <a href="{{ route('panel') }}" class="bg-white p-6 rounded-lg shadow hover:shadow-lg transition">
-                <h3 class="text-lg font-bold">Panel</h3>
+                <h3 class="text-lg font-bold">📊 Panel</h3>
                 <p class="text-gray-600">Venta Diaria</p>
             </a>
 
             <a href="{{ route('reportes') }}" class="bg-white p-6 rounded-lg shadow hover:shadow-lg transition">
-                <h3 class="text-lg font-bold">Reportes</h3>
+                <h3 class="text-lg font-bold">📈 Reportes</h3>
                 <p class="text-gray-600">Ventas y estadísticas</p>
             </a>
 
